b511bc104e3ecfe367583c5a543b23ea08e74553
Damian Nowakowski <damiano@Air-Damiano.local>
2026-04-14 17:57:17 +0100

    Traffic Simulation Project

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Traffic Simulation starting...\n";
+
+    
+    while (true) {
+        // simulation loop will go here later
+    }
+
+    return 0;
+}
